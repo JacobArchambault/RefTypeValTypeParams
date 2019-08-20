f67811d56fd851f8cfde529db0bbeefe61bb8642
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-20 12:08:14 -0400

    Add SendAPersonByValue method to main

--- RefTypeValTypeParams/Program.cs
+++ RefTypeValTypeParams/Program.cs
@@ -10,6 +10,16 @@ namespace RefTypeValTypeParams
     {
         static void Main(string[] args)
         {
+
+        }
+
+        static void SendAPersonByValue(Person p)
+        {
+            // Change the age of "p"?
+            p.personAge = 99;
+
+            // Will the caller see this reassignment?
+            p = new Person("Nikki", 99);
         }
     }
 }
